403766294c6104612569b3e9cdb25c494e47c2f4
uiseop <bono521@naver.com>
2022-08-07 00:17:26 +0900

    Refactore #12 수정된 Detail 페이지 수정 시작

--- components/Maps/MapLayout.tsx
+++ components/Maps/MapLayout.tsx
@@ -3,14 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { ReactNode } from 'react'
-import {
-  ButtonsWrapper,
-  NavWrapper,
-  WhiteLink,
-  WhiteLinkWrapper
-} from '../Home/styles/NavStyles'
 import Map from './Map'
-import { MainWrapper, SearchDetailInput } from './styles/styles'
+import { MainWrapper } from './styles/styles'
 
 interface MapLayoutProps {
   children: ReactNode
@@ -26,28 +20,6 @@ const MapLayout = ({ children }: MapLayoutProps) => {
       <Head>
         <title>카페인 | 지도</title>
       </Head>
-      <NavWrapper>
-        <Link href="/">
-          <a>
-            <Image
-              src={'/images/logo_black.svg'}
-              alt="카페인 로고"
-              width={80}
-              height={21.5}
-            />
-          </a>
-        </Link>
-        {cafeId && (
-          <SearchDetailInput placeholder="카페 이름이나 지하철역을 검색해보세요" />
-        )}
-        <ButtonsWrapper>
-          <WhiteLinkWrapper>
-            <Link href="/">
-              <WhiteLink>의견 보내기</WhiteLink>
-            </Link>
-          </WhiteLinkWrapper>
-        </ButtonsWrapper>
-      </NavWrapper>
       <MainWrapper>{children}</MainWrapper>
       <Map />
     </>
