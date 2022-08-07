3b58620ad665994728ede249fdcac25006175d4b
uiseop <bono521@naver.com>
2022-08-07 11:07:52 +0900

    fix #13 디테일 페이지 이미지 레이아웃 5 -> 3 변경

--- components/MapsParams/ImageSection.tsx
+++ components/MapsParams/ImageSection.tsx
@@ -11,7 +11,7 @@ const ImageSection = () => {
         <>
           {cafeInfo.storeImageList.length > 0 ? (
             <ImageWrappers>
-              {cafeInfo.storeImageList.slice(0, 5).map((imgData, idx) => (
+              {cafeInfo.storeImageList.slice(0, 3).map((imgData, idx) => (
                 <Image
                   src={imgData.imageUrl}
                   alt="카페 사진"
--- components/MapsParams/styles/ImageSectionStyles.tsx
+++ components/MapsParams/styles/ImageSectionStyles.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components'
 export const ImageWrappers = styled.div`
   display: grid;
   max-width: 680px;
-  height: 284px;
+  height: 242px;
   grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: 2.8fr 1fr 1fr;
-  grid-gap: 4px;
+  grid-template-columns: 2.316fr 1fr;
+  grid-gap: 2px;
 
   span:nth-child(1) {
     grid-row: 1 / 3;
