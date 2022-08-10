676ce609971f4d753289d5f0fd0a4f65242388cf
uiseop <bono521@naver.com>
2022-08-11 00:29:32 +0900

    Fix UI #5 지도 영역 위치 조절

--- components/Maps/styles/MapStyle.tsx
+++ components/Maps/styles/MapStyle.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 export const MapBox = styled.div`
-  width: 100vw;
-  height: calc(100vh - ${(props) => props.theme.marginTop.navHeight}px);
+  position: absolute;
+  left: 400px;
+  width: calc(100vw - 400px);
+  height: 100vh;
 `
