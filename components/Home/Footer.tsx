de6916f706cc10b3c5710e9c06766b3375344ab6
uiseop <bono521@naver.com>
2022-10-04 23:27:40 +0900

    Feat #25 Link href 설정

--- components/Home/Footer.tsx
+++ components/Home/Footer.tsx
@@ -58,7 +58,7 @@ const Footer = ({ isHome }: IFooterProps) => {
         </QItem>
         <QItem>
           <a
-            href="https://www.notion.so/cafeinofficial/84d615bf6c654196853bbc3644968135"
+            href="https://www.notion.so/cafeinofficial/b30868ccdcdd41ed97dde0c0b6f793dc"
             target="_blank"
             rel="noreferrer"
           >
@@ -67,7 +67,7 @@ const Footer = ({ isHome }: IFooterProps) => {
         </QItem>
         <QItem>
           <a
-            href="https://www.notion.so/cafeinofficial/84d615bf6c654196853bbc3644968135"
+            href="https://www.notion.so/cafeinofficial/1eac09f4c37341b793198f824c97e11e"
             target="_blank"
             rel="noreferrer"
           >
