a089dff49a6fc5adbe1dee28a87dd8c46bd14665
uiseop <bono521@naver.com>
2022-10-05 21:11:16 +0900

    Fix #25 set Image optimized false to fast loading

--- components/Maps/DetailImageSection.tsx
+++ components/Maps/DetailImageSection.tsx
@@ -14,7 +14,7 @@ import Ic_close from '@public/close.svg'
 import Ic_left_arrow from '@public/left_arrow_img.svg'
 import Ic_right_arrow from '@public/right_arrow_img.svg'
 import Ic_water from '@public/water.svg'
-import Image from 'next/image'
+import Image, { ImageProps } from 'next/image'
 import { useAtom, useAtomValue } from 'jotai'
 import { cafeInfoAtom, ImageListInterface, moreAtom } from 'store'
 import { useEffect, useRef, useState } from 'react'
@@ -24,21 +24,40 @@ import useWindowSize from 'hooks/useWindowSize'
 import preventDefault from '@utils/preventDefault'
 import { DimmedDetailImageWrapper } from '@components/common/Common'
 
-interface ImageProps {
+interface IDetailImageSection {
   isSingle: boolean
 }
 
-const DetailImageSection = ({ isSingle }: ImageProps) => {
+const DetailImageSection = ({ isSingle }: IDetailImageSection) => {
   const [more, setMore] = useAtom(moreAtom)
   const [imageId, setImageId] = useState(0)
   const router = useRouter()
   const slideRef = useRef<HTMLDivElement>(null)
   const store = useAtomValue(cafeInfoAtom)
+  const imageLists = []
   const { width, height } = useWindowSize()
   useEffect(() => {
     setMore(false)
     return () => setImageId(0)
   }, [router])
+
+  // useEffect(() => {
+  //   const config = {
+  //     width: 480,
+  //     height: 480,
+  //     alt: '카페 사진',
+  //     placeholder: 'blur',
+  //     blurDataURL: madeURL(480, 480)
+  //   }
+  //   if (store?.storeImageList) {
+  //     store.storeImageList.forEach((img) => {
+  //       // const img_config = { ...config, src: img.imageUrl }
+  //       const image = new Image()
+  //       image.src = img.imageUrl
+  //     })
+  //   }
+  // }, [store])
+
   const handleClick = () => {
     setMore(false)
     setImageId(0)
@@ -74,10 +93,11 @@ const DetailImageSection = ({ isSingle }: ImageProps) => {
                   src={`${store?.storeImageList[imageId].imageUrl}`}
                   width={480}
                   height={480}
-                  // layout={'fill'}
+                  // layout={'responsive'}
                   alt={'카페 사진'}
                   placeholder="blur"
                   blurDataURL={madeURL(480, 480)}
+                  unoptimized={true}
                 />
                 <Ic_water />
               </MainImage>
@@ -97,11 +117,12 @@ const DetailImageSection = ({ isSingle }: ImageProps) => {
                           src={storeImage.imageUrl}
                           width={100}
                           height={100}
-                          // layout={'fill'}
+                          // layout={'responsive'}
                           alt={'카페 이미지'}
                           priority
                           placeholder="blur"
                           blurDataURL={madeURL(100, 100)}
+                          unoptimized={true}
                           onClick={() => {
                             setImageId(idx)
                             slideRef.current?.scrollTo(idx * 110, 0)
