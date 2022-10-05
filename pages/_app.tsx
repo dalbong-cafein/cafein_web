e5f0c261ac8309bf2319392b625458d09a1f6445
uiseop <bono521@naver.com>
2022-10-05 12:23:40 +0900

    Feat #31 Hot jar, Google Analitics 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import { Props } from 'next/script'
-import { ReactElement, ReactNode } from 'react'
+import { useRouter } from 'next/router'
+import Script, { Props } from 'next/script'
+import { ReactElement, ReactNode, useEffect } from 'react'
+import { hotjar } from 'react-hotjar'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '../styles/global-style'
 import { theme } from '../styles/theme'
+import * as gtag from 'lib/gtag'
 
 // 잘 모르고 사용한 것들. getStaticProps의 Prop들의 타입을 지정해주는 함수인 것 같음
 export type NextPageWithLayout<P = any, IP = P> = NextPage<P, IP> & {
@@ -17,8 +20,42 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  useEffect(() => {
+    hotjar.initialize(3184414, 6)
+  }, [])
   return (
     <ThemeProvider theme={theme}>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
       <GlobalStyle />
       {getLayout(<Component {...pageProps} />, pageProps)}
     </ThemeProvider>
